3c93f3621dd8340b076eea29d0a84fb123302872
OksanaBakl <oksanamolesha@ukr.net>
2022-01-27 22:07:33 +0200

    added ctrls

---
+++ controllers/contacts/add.js
@@ -0,0 +1,21 @@
+const { BadRequest } = require("http-errors");
+const { Contact, joiSchema } = require("../../models/contact");
+
+const add = async (req, res, next) => {
+	try {
+		const { error } = joiSchema.validate(req.body);
+		if (error) {
+			throw new BadRequest(error.message);
+		}
+		const { _id } = req.user;
+		const newContact = await Contact.create({ ...req.body, owner: _id });
+		res.status(201).json(newContact);
+	} catch (error) {
+		if (error.message.includes("validation failed")) {
+			error.status = 400;
+		}
+		next(error);
+	}
+};
+
+module.exports = add;
---
+++ controllers/contacts/removeById.js
@@ -0,0 +1,17 @@
+const { Contact } = require("../../models/contact");
+
+const removeById = async (req, res, next) => {
+	const { id } = req.params;
+	try {
+		const deleteContact = await Contact.findByIdAndRemove(id);
+		if (!deleteContact) {
+			const error = new Error("not found");
+			error.status = 404;
+			throw error;
+		}
+		res.json({ message: "contact deleted" });
+	} catch (error) {
+		next(error);
+	}
+};
+module.exports = removeById;
